073ecf41a3900add6572fcb717c833b3157e7f0a
a.bayakhmetov <a.bayakhmetov@kdss.tech>
2024-08-26 15:14:39 +0500

    added changes on preview

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get('/', async (req, res) => {
         </head>
         <body>
             <h1>Custom Link Preview Example</h1>
-            <img src="${imageUrl}" alt="Example Image" style="max-width: 100%;">
+            <img src="https://lh4.googleusercontent.com/proxy/wGqLYEKOXLPkiVvh42G8s9KU6TLTYiwe8K1tOU6QkKKWWPdV3XFFT_fDE8HyJjVKDjS4oJLPf1oyK-H7-BCZIKb_jx5gPLnjCbNzsVi_CQVXHWqpXSg" alt="Example Image" style="max-width: 100%;">
         </body>
         </html>
     `);
